48b5f069a83c58260862cf528deede35b97cbe4d
rmedina09 <raulmp@ciencias.unam.mx>
2017-12-15 18:56:37 -0600

    Version estable para calcular percentiles 1,5,10,25,50,75,90,95,99,99.9

--- Alerts/Step1/histograma_ncl_paralelo_olmozavala_v1.sh
+++ Alerts/Step1/histograma_ncl_paralelo_olmozavala_v1.sh
@@ -9,11 +9,11 @@
 #############################################
 
 #!/bin/bash
-#SBATCH -J Percentiles_NCL_OlmoZavala 
+#SBATCH -J Histogramas_NCL_OlmoZavala 
 #SBATCH -p id
-#SBATCH -N 1 # Numero de Nodos 
+#SBATCH -N 5 # Numero de Nodos 
 #SBATCH --ntasks-per-node 24 # Número de tareas por nodo
-#SBATCH -t 0-2:00 # Tiempo (D­HH:MM) 
+#SBATCH -t 0-24:00 # Tiempo (D­HH:MM) 
 #SBATCH -o slurm.%x.%j.out # STDOUT Salida estandar (tag name,id)
 #SBATCH -e slurm.%x.%j.err # STDERR Error estándar (tag name,id)
 
@@ -21,9 +21,9 @@
 export NCARG_ROOT=/opt/librerias/pgi/ncl-6.4.0
 export PATH=/opt/librerias/pgi/ncl-6.4.0/bin:$PATH
 
-N=23
+N=119
 for i in $(seq 0 $N);do
-	time srun -n1 --exclusive ncl md=$i N=$N histo_paralelo.ncl &
+	time srun -N1 -n1 --exclusive ncl md=$i N=$N histo_paralelo.ncl &
 done
 
 wait
