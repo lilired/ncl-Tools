f2a321c28645e770a084e1252bd774a11b40027d
mijailcc <mvagvcc@gmail.com>
2018-06-18 17:23:54 -0500

    se agrega el readme de la generacion de alertas

--- Alerts/generation_maps_alert/Alertas.py
+++ Alerts/generation_maps_alert/Alertas.py
@@ -4,31 +4,31 @@ import sys
 import os
 import re
 import time
-import numpy as np
+import gc
 from netCDF4 import Dataset
 from datetime import datetime
 from datetime import timedelta
+import numpy as np
 from scipy import interpolate
-import gc
 
 
 #constantes de archivo
 TOKENFILE = "/"
 MONTHS = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
 #constantes para T2
-T2VARS = ["T2_10", "T2_5", "T2_1", "T2_95", "T2_99", "T2_99_9"]
-T2VALUES = [-1, -2, -3, 1, 2, 3]#el orden si importa
+T2VARS = ["T2_5", "T2_1", "T2_0_1", "T2_95", "T2_99", "T2_99_9"]#el orden si importa
+T2VALUES = [5, 1, 0.1, 95, 99, 99.9]#el orden si importa
 #constantes para viento
-WINVARS = ["VIENTO_10", "VIENTO_5", "VIENTO_1", "VIENTO_95", "VIENTO_99", "VIENTO_99_9"]
-WINVALUES = [0, 0, 0, 1, 2, 3]#el orden si importa
+WINVARS = ["VIENTO_5", "VIENTO_1", "VIENTO_0_1", "VIENTO_95", "VIENTO_99", "VIENTO_99_9"]#el orden si importa
+WINVALUES = [50, 50, 50, 95, 99, 99.9]#el orden si importa
 #constantes para precipitacion
-PRECVARS = ["PREC_10", "PREC_5", "PREC_1", "PREC_95", "PREC_99", "PREC_99_9"]#verificar
-PRECVALUES = [0, 0, 0, 1, 2, 3]#el orden si importa
-#constantes para tiempo
+PRECVARS = ["PREC_5", "PREC_1", "PREC_0_1", "PREC_95", "PREC_99", "PREC_99_9"]#verificar
+PRECVALUES = [50, 50, 50, 95, 99, 99.9]#el orden si importa
+#constantes para el tiempo
 FORMAT = "%Y-%m-%d"
 CALENDAR = "gregorian"
-SINCE = "hours since 2017-11-26 00:00:00"
-#dimenciones de latitud y longitus
+SINCE = "hours since 2017-11-26 00:00:00"#si no se especifica una esta se ocupa por omision
+#dimenciones de latitud y longitud
 DIMLAT = 262
 DIMLON = 338
 DIMLAT_2 = 156
@@ -42,7 +42,10 @@ def get_file_day(dom, path):
     today = datetime.today()
     year = today.year
     mount = today.month
-    nfile = path + TOKENFILE + str(year) + TOKENFILE + str(mount) \
+    smount = str(mount)
+    if mount < 10:
+        smount = "0" + smount
+    nfile = path + TOKENFILE + str(year) + TOKENFILE + smount \
             + "_" + MONTHS[mount - 1]  + TOKENFILE + "wrfout_d0" \
             + str(dom) + "_" + today.strftime(FORMAT) + "_00.nc"#direccion del archivo path/año/mesEntero_mesCadena/wrfout_d0(dominio 1 o 2)_(año_mes_dia)_00.nc
     if not os.path.isfile(nfile):
@@ -50,12 +53,14 @@ def get_file_day(dom, path):
     print("Forecast file: " + nfile)
     return Dataset(nfile, "r", format="NETCDF4")
 
+#obtine el archivo de percentiles
 def get_file_percentiles(pvars, dom, path):
     per = {}
     if dom != 1:
         dom = 2
-    nfile = path + TOKENFILE + "Percentil_Dom" + str(dom) \
-            + "_" + datetime.today().strftime(FORMAT) + ".nc"#path/Percentil_Dom(dominio 1 o 2)_(año_mes_dia).nc
+    #nfile = path + TOKENFILE + "Percentil_Dom" + str(dom) \
+    #        + "_" + datetime.today().strftime(FORMAT) + ".nc"#path/Percentil_Dom(dominio 1 o 2)_(año_mes_dia).nc
+    nfile = path + TOKENFILE + "Percentil_Dom" + str(dom) + ".nc"
     if not os.path.isfile(nfile):
         raise IOError("percentiles file not found ", nfile)
     print("percentiles file: " + nfile)
@@ -67,13 +72,13 @@ def get_file_percentiles(pvars, dom, path):
 #obtiene una variable del archivo netcdf
 def get_var_netcdf(nameVar, netCDF):
     if nameVar in netCDF.variables:
-    	return np.copy(netCDF.variables.get(nameVar))
-    else:
-    	print("variable not found: " + nameVar)
+        return np.copy(netCDF.variables.get(nameVar))
+    raise ValueError("variable not found: " + nameVar)
 
 #obtiene los valores de alerta segun la variable
 def alert_values(forecast, percentiles, percentilesValues):
-    aler = np.zeros(forecast.shape, dtype=int)
+    aler = np.empty(forecast.shape)
+    aler.fill(50)
     k = 0
     for dayforecast in forecast:#itera sobre las horas
         i = 0
@@ -81,7 +86,7 @@ def alert_values(forecast, percentiles, percentilesValues):
             percentil = percentiles[ky]
             if(len(percentil.shape) == (len(dayforecast.shape) + 1)):
                 percentil = percentil[0]
-            if(percentilesValues[i] < 0):
+            if(percentilesValues[i] < 50):
                 aler[k][dayforecast < percentil] = percentilesValues[i]
             else:
                 aler[k][percentil < dayforecast] = percentilesValues[i]
@@ -116,7 +121,7 @@ def create_netcdf_file(name, varsNCDF, dimensions, ow=None):
     gc.collect()
     print("Close file")
     
-#obtiene los valores de las variables de longitud y Latitud
+#crea los valores de las variables de longitud y Latitud para el formato netcdf
 def netcdf_var_latlon(dom, lat, lon, fillLat=None, fillLon=None):
     net = {}
     if dom != 1:
@@ -136,14 +141,30 @@ def netcdf_var_latlon(dom, lat, lon, fillLat=None, fillLon=None):
                                         "standard_name": "longitude"}}
     return net
 
-#obtiene la dimencion de las variables latitud y longitudlatlong
+
+
+ ["T2_5", "T2_1", "T2_0_1", "T2_95", "T2_99", "T2_99_9"
+PYTHON="/usr/local/anaconda/bin/python"
+ROOT="/home/vladimir/CENAPRED/Alertas"
+PERCENTILES="Alertas.py"
+
+
+#archivo donde se encuentran los percentiles
+PER_FILE="/ServerData/OutTempRaul/percentil/Percentiles.nc"
+#archivo donde se encuentran los pronosticos
+FCT_FILE="/ServerData/Pronosticos/Salidas/WRF"
+#archivo de salida
+OUT=""
+
+
+#crea la dimencion de las variables latitud y longitud para el formato netcdf
 def netcdf_dim_latlon(dom, lat=DIMLAT, lon=DIMLON):
     if dom != 1:
         lat = DIMLAT_2
         lon = DIMLON_2
     return {"Latitude": {"size": lat}, "Longitude": {"size": lon}}
 
-#obtiene la variable tiempo
+#crea la variable tiempo para el formato netcdf
 def netCDFVarTime(time, since=SINCE, calendar=CALENDAR, fill=None):
     return {"Time" : {"type" : "f4",
                       "depends" : ("Time",),
@@ -162,52 +183,52 @@ def netCDFVarTime(time, since=SINCE, calendar=CALENDAR, fill=None):
                                        "long_name": "Time", 
                                        "standard_name": "time"}}}
 
-#obtiene la dimencion de la variable tiempo
+#crea la dimencion de la variable tiempo para el formato netcdf
 def netcdf_dim_time(time=None):
     return {"Time": {"size": time},
             "OTime" : {"size": None}}
 
-#crea una variable T2 con los valores -1,-2,-3,1,2,3, dependiendo si pasa algun percentil
+#crea una variable T2 con los valores -1,-2,-3,1,2,3, dependiendo si sobrepasa o esta por debajo de algun percentil
 def netcdf_var_t2(value, value2, fill=None):
-    return {"T2" : {"type" : "i4", 
+    return {"T2" : {"type" : "f4", 
                     "depends" : ("Time", "Latitude", "Longitude", ), 
                     "value" : value, 
                     "fill": fill, 
-                    "attributes" : {"units": "C", 
+                    "attributes" : {"units": "%", 
                                     "descripcion": "TEMP at 2M", 
                                     "long_name": "TEMP a 2M", 
                                     "standard_name": "TEMP a 2M"}}, 
-            "T2A" : {"type" : "i4", 
+            "T2A" : {"type" : "f4", 
                      "depends" : ("OTime", "Latitude", "Longitude", ), 
                      "value" : value2, 
                      "fill": fill,
-                     "attributes" : {"units": "C", 
+                     "attributes" : {"units": "%", 
                                      "descripcion": "TEMP at 2M", 
                                      "long_name": "TEMP a 2M", 
                                      "standard_name": "TEMP a 2M"}}}
 
-#crea una variable VIENTO con los valores 0,0,0,1,2,3, dependiendo si pasa algun percentil
+#crea una variable VIENTO con los valores 0,0,0,1,2,3, dependiendo si sobrepasa o esta por debajo de algun percentil
 def netcdf_var_win(value, value2, fill=None):
-    return {"VIENTO" : {"type" : "i4", 
+    return {"VIENTO" : {"type" : "f4", 
                         "depends" : ("Time", "Latitude", "Longitude", ), 
                         "value" : value, 
                         "fill": fill, 
-                        "attributes" : {"units": "m s-1", 
+                        "attributes" : {"units": "%", 
                                         "descripcion": "Magnitud del viento", 
                                         "long_name": "Magnitud del viento", 
                                         "standard_name": "Magnitud del viento"}},
-            "VIENTOA" : {"type" : "i4", 
+            "VIENTOA" : {"type" : "f4", 
                          "depends" : ("OTime", "Latitude", "Longitude", ), 
                          "value" : value2, 
                          "fill": fill, 
-                         "attributes" : {"units": "m s-1", 
+                         "attributes" : {"units": "%", 
                                          "descripcion": "Magnitud del viento", 
                                          "long_name": "Magnitud del viento", 
                                          "standard_name": "Magnitud del viento"}}}
 
-#crea una variable PREC con los valores 0,0,0,1,2,3, dependiendo si pasa algun percentil
+#crea una variable PREC con los valores 0,0,0,1,2,3, dependiendo si pasa sobrepasa o esta por debajo de algun percentil
 def netcdf_var_prec(value, value2, fill=None):
-    return {"PREC" : {"type" : "i4", 
+    return {"PREC" : {"type" : "f4", 
                       "depends" : ("Time", "Latitude", "Longitude", ), 
                       "value" : value, 
                       "fill": fill, 
@@ -215,7 +236,7 @@ def netcdf_var_prec(value, value2, fill=None):
                                       "descripcion": "Precipitacion", 
                                       "long_name": "Precipitacion", 
                                       "standard_name": "Precipitacion"}}, 
-            "PRECA" : {"type" : "i4", 
+            "PRECA" : {"type" : "f4", 
                        "depends" : ("OTime", "Latitude", "Longitude", ), 
                        "value" : value2, 
                        "fill": fill, 
@@ -225,10 +246,11 @@ def netcdf_var_prec(value, value2, fill=None):
                                        "standard_name": "Precipitacion"}}}
 
 
+#crea una nueva dimencion en un archivo netcdf
 def netcdf_dim(netcdf, name, size=None):
     netcdf.createDimension(name, size)
 
-#se definen una funcion
+#crea una nueva variable en un archivo netcdf
 def netcdf_var(netcdf, name, typ, depends, value, attributes, fill=None):
     var = netcdf.createVariable(name, typ, depends, fill)
     var[:] = value
@@ -243,6 +265,9 @@ def alert_t2(spath, dom, fpath, ppath):
     #percentiles = get_file_percentiles(T2VARS, dom, ppath)
     percentiles = estep2(ppath, T2VARS, get_var_netcdf("XLAT", netcdf)[0,::,0], get_var_netcdf("XLONG", netcdf)[0][0,:], "T2_dom" + str(dom))
     alert = alert_values(forecast, percentiles, T2VALUES)
+    if dom == 1:
+        alert[:,0:50,:] = 50
+        alert[:,222:,:] = 50
     var = {}
     var.update(netCDFVarTime(np.arange(121), datetime.today().strftime(FORMAT) + " 00:00:00"))
     var.update(netcdf_var_latlon(dom, get_var_netcdf("XLAT", netcdf)[0,::,0], get_var_netcdf("XLONG", netcdf)[0][0,:]))
@@ -261,6 +286,9 @@ def alert_wind(spath, dom, fpath, ppath):
     #percentiles = get_file_percentiles(WINVARS, dom, ppath)
     percentiles = estep2(ppath, WINVARS, get_var_netcdf("XLAT", netcdf)[0,::,0], get_var_netcdf("XLONG", netcdf)[0][0,:], "VIENTO_dom" + str(dom))
     alert = alert_values(forecast, percentiles, WINVALUES)
+    if dom == 1:
+        alert[:,0:50,:] = 50
+        alert[:,222:,:] = 50
     var = {}
     var.update(netCDFVarTime(np.arange(121), datetime.today().strftime(FORMAT) + " 00:00:00"))
     var.update(netcdf_var_latlon(dom, get_var_netcdf("XLAT", netcdf)[0,::,0], get_var_netcdf("XLONG", netcdf)[0][0,:]))
@@ -270,13 +298,40 @@ def alert_wind(spath, dom, fpath, ppath):
     dim.update(netcdf_dim_time())
     create_netcdf_file(spath + "VIENTO_dom" + str(dom) + "_" +  datetime.today().strftime(FORMAT) + ".nc", var, dim)
 
+#crea las alertas para la precipitacion
+def alert_prec(spath, dom, fpath, ppath):
+    netcdf = get_file_day(dom, fpath)
+    RAINC = get_var_netcdf("RAINC", netcdf)
+    RAINNC = get_var_netcdf("RAINNC", netcdf)
+    forecast = RAINC + RAINNC
+    vrange = range(0, 120, 24)
+    for i in range(0, len(vrange) - 1):
+        j = vrange[i + 1]
+        res = j - 1
+        forecast[j:] = forecast[j:] - forecast[res]
+    #percentiles = get_file_percentiles(WINVARS, dom, ppath)
+    percentiles = estep2(ppath, PRECVARS, get_var_netcdf("XLAT", netcdf)[0,::,0], get_var_netcdf("XLONG", netcdf)[0][0,:], "PREC_dom" + str(dom))
+    alert = alert_values(forecast, percentiles, PRECVALUES)
+    if dom == 1:
+        alert[:,0:50,:] = 50
+        alert[:,222:,:] = 50
+    var = {}
+    var.update(netCDFVarTime(np.arange(121), datetime.today().strftime(FORMAT) + " 00:00:00"))
+    var.update(netcdf_var_latlon(dom, get_var_netcdf("XLAT", netcdf)[0,::,0], get_var_netcdf("XLONG", netcdf)[0][0,:]))
+    var.update(netcdf_var_prec(alert, np.array([np.amax(alert, axis=0)])))
+    dim = {}
+    dim.update(netcdf_dim_latlon(dom))
+    dim.update(netcdf_dim_time())
+    create_netcdf_file(spath + "PREC_dom" + str(dom) + "_" +  datetime.today().strftime(FORMAT) + ".nc", var, dim)
 
-#funcion que ajusta los puntos interpolando los os.listdir(path + sYear)dados por raul 
+
+
+#funcion que ajusta los puntos interpolando dados por Raul 
 def estep2(path, pvars,  lat, lon, name):
     if not os.path.isfile(path):
         raise IOError("main percentiles file not found ", path)
     netcdf = Dataset(path, "r", format="NETCDF4")
-    wnetcdf = Dataset("aux_" + name + ".nc", "w", format="NETCDF4")
+    wnetcdf = Dataset(name + ".nc", "w", format="NETCDF4")
     netcdf_dim(wnetcdf, "Latitude", lat.shape[0])
     netcdf_var(wnetcdf, "Latitude", "f4", ("Latitude",), lat, {})
     netcdf_dim(wnetcdf, "Longitude", lon.shape[0])
@@ -286,45 +341,16 @@ def estep2(path, pvars,  lat, lon, name):
     data = {}
     x = get_var_netcdf("Latitude", netcdf)
     y = get_var_netcdf("Longitude", netcdf)
-    print("lat: " + str(x.shape) + " lon: " + str(y.shape))
-    print("nlat: " + str(lat.shape) + " nlon: " + str(lon.shape))
     for var in pvars:
         vvar = get_var_netcdf(var, netcdf)
         f = interpolate.interp2d(x, y, np.transpose(vvar), kind='linear')
         data[var] = np.transpose(f(lat, lon))
-        print(data[var].shape)
         netcdf_var(wnetcdf, var, "f4", ("Time", "Latitude", "Longitude", ), np.array([data[var]]), {})
     netcdf.close()
     wnetcdf.close()
     return data
 
-def copy(fFile, target, sFile=None):
-    dsinf = Dataset(fFile, "r", format="NETCDF4")
-    if not sFile is None:
-        dsins = Dataset(sFile, "r", format="NETCDF4")
-    dsout = Dataset(target, "w", format="NETCDF4")
-    for dname, the_dim in dsinf.dimensions.iteritems():
-        print(dname + " dim  " + str(len(the_dim)))
-        dsout.createDimension(dname, len(the_dim) if not the_dim.isunlimited() else None)
-    for v_name, varin in dsinf.variables.iteritems():
-        outVar = dsout.createVariable(v_name, varin.datatype, varin.dimensions)
-        print(varin.datatype)
-        outVar.setncatts({k: varin.getncattr(k) for k in varin.ncattrs()})
-        outVar[:] = varin[:]
-    if not sFile is None:
-        for dname, the_dim in dsins.dimensions.iteritems():
-            print(dname + " dim  " + str(len(the_dim)))
-            if not dname in dsinf.dimensions.iteritems():
-                dsout.createDimension(dname, len(the_dim) if not the_dim.isunlimited() else None)
-        for v_name, varin in dsins.variables.iteritems():
-            if not v_name in dsinf.variables.iteritems():
-                outVar = dsout.createVariable(v_name, varin.datatype, varin.dimensions)
-                print(varin.datatype)
-                outVar.setncatts({k: varin.getncattr(k) for k in varin.ncattrs()})
-                outVar[:] = varin[:]
-    dsout.close()
-
-#elimina los archivos que se crearon un dia anterior
+#elimina los archivos que se crearon un dia anterior, no es necesario en el cluster
 def remove(path):
     if path == "":
         path = os.getcwd()
@@ -343,10 +369,26 @@ def main(argv):
         #archivo donde se guarda el archivo(spath), ruta donde se encuentra el archivo de pronostico(fpath), ruta donde se encuentra el archivo de percentiles, respectivamente(ppath)
         #argv[1], argv[2], argv[3]
         remove(argv[1])
+        #Genera los archivos de salida del step 2, solo necesarios la primera vez
+        #lat1 = np.loadtxt("lat1.txt")
+        #lon1 = np.loadtxt("lon1.txt")
+        #lat2 = np.loadtxt("lat2.txt")
+        #lon2 = np.loadtxt("lon2.txt")
+        #alvars = []
+        #alvars.extend(T2VARS)
+        #alvars.extend(WINVARS)
+        #alvars.extend(PRECVARS)
+        #dominio 1
+        #percentiles = estep2(argv[3], alvars, lat1, lon1, "Percentil_Dom1")
+        #dominio 2
+        #percentiles = estep2(argv[3], alvars, lat2, lon2, "Percentil_Dom2")
+        #crea las alertas
         alert_t2(argv[1], 1, argv[2], argv[3])#para el dominio 1
         alert_t2(argv[1], 2, argv[2], argv[3])#para el dominio 2
         alert_wind(argv[1], 1, argv[2], argv[3])#para el dominio 1
         alert_wind(argv[1], 2, argv[2], argv[3])#para el dominio 2
+        alert_prec(argv[1], 1, argv[2], argv[3])#para el dominio 1
+        alert_prec(argv[1], 2, argv[2], argv[3])#para el dominio 2
 
 if __name__ == "__main__":
     main(sys.argv)
